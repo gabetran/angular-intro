b53c6935e3fe00b4c21b18396f9700155f4b8c4e
Gabe Tran <gabetran@uw.edu>
2014-11-04 09:58:02 -0800

    learning angular

--- js/app.js
+++ js/app.js
@@ -4,4 +4,8 @@
  */
 
 "use strict";
-
+angular.module('MoviesApp', [])
+	.controller('MoviesController', function($scope){
+		//intialize variables with list of movies such that it'll be accessed in the view
+		$scope.movies = movies;
+	})
