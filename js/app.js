a9420ca8f55b61efaad03e74e72df896491a38fe
Gabe Tran <gabetran@uw.edu>
2014-11-04 10:15:25 -0800

    learning angular

--- js/app.js
+++ js/app.js
@@ -8,4 +8,21 @@ angular.module('MoviesApp', [])
 	.controller('MoviesController', function($scope){
 		//intialize variables with list of movies such that it'll be accessed in the view
 		$scope.movies = movies;
-	})
+
+		//sorting columns
+		$scope.sortCol = 'rank';
+		$scope.sortReverse = false;
+
+		$scope.sortBy = function(colName){
+			if($scope.sortCol == colName){
+				$scope.sortReverse = !$scope.sortReverse;
+			}else{
+				$scope.sortReverse = false;
+				$scope.sortCol = colName;
+			}
+		};
+
+		$scope.isSortedBy = function(colName) {
+			return $scope.sortCol == colName;
+		};
+	});
